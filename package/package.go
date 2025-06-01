7b2ad56991b6c89cf2e9631ccc03ccf912073f79
jurgen.kluft <jurgen.kluft@gmail.com>
2025-06-01 08:33:54 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -3,34 +3,41 @@ package ctime
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	denv "github.com/jurgen-kluft/ccode/denv"
-	ccore "github.com/jurgen-kluft/ccore/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
-// GetPackage returns the package object of 'ctime'
+const (
+	repo_path = "github.com\\jurgen-kluft"
+	repo_name = "ctime"
+)
+
 func GetPackage() *denv.Package {
-	// Dependencies
+	name := repo_name
+
+	// dependencies
 	cunittestpkg := cunittest.GetPackage()
-	ccorepkg := ccore.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
-	// The main (ctime) package
-	mainpkg := denv.NewPackage("ctime")
+	// main package
+	mainpkg := denv.NewPackage(repo_path, repo_name)
 	mainpkg.AddPackage(cunittestpkg)
-	mainpkg.AddPackage(ccorepkg)
+	mainpkg.AddPackage(cbasepkg)
 
-	// 'ctime' library
-	mainlib := denv.SetupCppLibProject("ctime", "github.com\\jurgen-kluft\\ctime")
-	mainlib.AddDependencies(ccorepkg.GetMainLib()...)
+	// main library
+	mainlib := denv.SetupCppLibProject(mainpkg, name)
+	mainlib.AddDependencies(cbasepkg.GetMainLib()...)
 
-	// 'ctime' unittest project
-	maintest := denv.SetupDefaultCppTestProject("ctime_test", "github.com\\jurgen-kluft\\ctime")
+	// test library
+	testlib := denv.SetupCppTestLibProject(mainpkg, name)
+	testlib.AddDependencies(cunittestpkg.GetTestLib()...)
+
+	// unittest project
+	maintest := denv.SetupCppTestProject(mainpkg, name)
 	maintest.AddDependencies(cunittestpkg.GetMainLib()...)
-	maintest.AddDependencies(ccorepkg.GetMainLib()...)
-	maintest.AddDependencies(cbasepkg.GetMainLib()...)
-	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+	maintest.AddDependency(testlib)
 
 	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddTestLib(testlib)
 	mainpkg.AddUnittest(maintest)
 	return mainpkg
 }
